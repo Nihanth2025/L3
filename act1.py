82ff7e548ae20aa1a1382742c3530779c1db7675
Nihanth2025 <sk1441@gmail.com>
2025-04-07 15:57:46 +0530

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,11 @@
+a=15
+print("data type of a",type(a) )
+
+b=123.9567894123
+print("data type of b",type(b) )
+
+c=True
+print("data type of c",type(c) )
+
+d="apple"
+print("data type of d",type(d) )
---
+++ act2.py
@@ -0,0 +1,13 @@
+num1=10
+print("number1=",num1)
+num2=float(num1)
+print("number2",num2)
+
+num3=139.2369
+print("number3=",num3)
+num4=int(num3)
+print("number4",num4)
+
+age=int(input("Enter your age:"12))
+print("age is=",age)
+print("datatype of age:",type(age))
